f51280447af31bbf1c7d2f78384895724c94718f
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-06 11:30:43 -0500

    installs, folder setups, and server running

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const server = express();
+
+const actionRouter = require('./data/helpers/actionRouter');
+const postRouter = require('./data/helpers/projectRouter');
+
+function logger(req, res, next) {
+	console.log(`${req.method} to ${req.originalUrl} at ${new Date()}`);
+	next();
+}
+
+server.use(logger);
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send(`<h2>She works!!</h2>`);
+});
+
+module.exports = server;
